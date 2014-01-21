1179a5d2aa57d7a035318a4cb22279432ecb62ed
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2014-01-21 14:19:49 +0100

    Store files in session.

--- src/main/java/net/pterodactylus/ams/core/Session.java
+++ src/main/java/net/pterodactylus/ams/core/Session.java
@@ -1,5 +1,12 @@
 package net.pterodactylus.ams.core;
 
+import static java.util.Collections.unmodifiableCollection;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * A session binds together the execution of several {@link Command}s and can
  * be used to transport data from one command to another.
@@ -7,5 +14,15 @@ package net.pterodactylus.ams.core;
  * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
  */
 public class Session {
+
+	private final Set<File> files = new HashSet<>();
+
+	public void addFile(File file) {
+		files.add(file);
+	}
+
+	public Collection<File> getFiles() {
+		return unmodifiableCollection(files);
+	}
 
 }
--- src/test/java/net/pterodactylus/ams/core/SessionTest.java
+++ src/test/java/net/pterodactylus/ams/core/SessionTest.java
@@ -1,10 +1,29 @@
 package net.pterodactylus.ams.core;
 
+import static java.io.File.listRoots;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+
+import java.io.File;
+import java.util.Collection;
+
+import org.junit.Test;
+
 /**
  * Unit test for {@link Session}.
  *
  * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
  */
 public class SessionTest {
+
+	private final Session session = new Session();
+
+	@Test
+	public void sessionCanStoreFiles() {
+		File root = listRoots()[0];
+		session.addFile(root);
+		Collection<File> files = session.getFiles();
+		assertThat(files, contains(root));
+	}
 
 }
