8b754bfb8baaa291f2f2ffb1ae6cd6d081629e66
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2014-01-22 00:24:43 +0100

    Add name to commands.

--- src/main/java/net/pterodactylus/ams/core/Command.java
+++ src/main/java/net/pterodactylus/ams/core/Command.java
@@ -9,6 +9,10 @@ import java.io.IOException;
  */
 public interface Command {
 
+	default String getName() {
+		return getClass().getSimpleName().replaceAll("Command$", "");
+	}
+
 	void process(Session session) throws IOException;
 
 }
--- src/test/java/net/pterodactylus/ams/core/CommandProcessorTest.java
+++ src/test/java/net/pterodactylus/ams/core/CommandProcessorTest.java
@@ -2,6 +2,7 @@ package net.pterodactylus.ams.core;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 
@@ -17,6 +18,7 @@ public class CommandProcessorTest {
 	@Test
 	public void canCallCommandWithSession() throws IOException {
 		Command command = mock(Command.class);
+		when(command.getName()).thenReturn("Test");
 		Session session = new Session();
 		CommandProcessor commandProcessor = new CommandProcessor(session);
 		commandProcessor.process(command);
