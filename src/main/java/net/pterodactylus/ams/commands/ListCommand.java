1810463420c797d7f91fb5027a545c38c58fa2bb
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2014-01-22 13:42:48 +0100

    Add filtering to list command.

--- src/main/java/net/pterodactylus/ams/commands/ListCommand.java
+++ src/main/java/net/pterodactylus/ams/commands/ListCommand.java
@@ -1,10 +1,12 @@
 package net.pterodactylus.ams.commands;
 
 import static java.lang.String.format;
+import static java.util.stream.Collectors.toList;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import net.pterodactylus.ams.core.Command;
 import net.pterodactylus.ams.core.Session;
@@ -19,8 +21,11 @@ public class ListCommand implements Command {
 
 	@Override
 	public void process(Session session, List<String> parameters) throws IOException {
+		List<Pattern> patterns = parameters.stream().map(Pattern::compile).collect(toList());
 		for (File file : session.getFiles()) {
-			session.getOutput().write(format("%s\n", file.getAbsolutePath()));
+			if (patterns.isEmpty() || patterns.stream().allMatch((pattern) -> pattern.matcher(file.getPath()).find())) {
+				session.getOutput().write(format("%s\n", file.getAbsolutePath()));
+			}
 		}
 	}
 
