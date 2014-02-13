e345a013da75ef29099e47e054cd1ae499b3d669
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2014-02-13 20:02:03 +0100

    Add tag remover interface.

---
+++ src/main/java/net/pterodactylus/util/tag/TagRemover.java
@@ -0,0 +1,26 @@
+package net.pterodactylus.util.tag;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * A tag remover can remove a single tag from a file.
+ * <p/>
+ * This interface needs to be implemented for every tag format.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public interface TagRemover {
+
+	/**
+	 * Removes a tag from the given file.
+	 *
+	 * @param file
+	 * 		The file to remove a tag from
+	 * @return {@code true} if a tag was removed, {@code false} otherwise
+	 * @throws IOException
+	 * 		if an I/O error occurs
+	 */
+	boolean removeTag(File file) throws IOException;
+
+}
